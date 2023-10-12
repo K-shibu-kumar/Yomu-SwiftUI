1cd5204742e28e5c123c4234a15f37763a1076cd
Dixon A <dixona31@gmail.com>
2023-10-13 00:30:21 +0530

    Updating HomeView

---
+++ Yomu/View/CircleGroupView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleGroupView.swift
+//  Yomu
+//
+//  Created by Dixon A on 13/10/23.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleGroupView()
+}
