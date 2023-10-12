1ead76682f1774c8b1548af836434d01128fbb2e
Dixon A <dixona31@gmail.com>
2023-10-12 19:18:13 +0530

    Initial Commit

---
+++ Yomu/YomuApp.swift
@@ -0,0 +1,17 @@
+//
+//  YomuApp.swift
+//  Yomu
+//
+//  Created by Dixon A on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct YomuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
